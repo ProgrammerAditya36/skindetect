67d91f567f0aecf3e45e9b04d9d393c8a5fb35a5
programmeraditya36@gmail.com <programmeraditya36@gmail.com>
2024-06-21 00:14:40 +0530

    Small_modifications

--- app/views.py
+++ app/views.py
@@ -3,8 +3,8 @@ import numpy as np
 from django.conf import settings
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing import image
+from tensorflow.keras.models import load_model # type: ignore
+from tensorflow.keras.preprocessing import image # type: ignore
 
 # Load the Keras model
 model_path = os.path.join(settings.BASE_DIR, 'model_checkpoint.weights.keras')
