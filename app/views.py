950ad1e9eb869bbafe33494fe8beef052ff1881b
programmeraditya36@gmail.com <programmeraditya36@gmail.com>
2024-06-21 00:13:47 +0530

    Basic_Backed_Done

--- app/views.py
+++ app/views.py
@@ -1,28 +1,41 @@
-from django.shortcuts import render
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
 import os
 import numpy as np
-from tensorflow.keras.models import load_model # type: ignore
-from tensorflow.keras.preprocessing import image # type: ignore
-# Create your views here.
-model_path = os.path.join(os.path.dirname(__file__), '../model_checkpoint.weights.keras')
-print(model_path)
-model = load_model(model_path)
+from django.conf import settings
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+
+# Load the Keras model
+model_path = os.path.join(settings.BASE_DIR, 'model_checkpoint.weights.keras')
+
+
 @csrf_exempt
 def predict_image(request):
-    if request.method == 'POST' and request.FILES['image']:
-        img = request.FILES['image']
-        img_path = os.path.join('media', img.name)
-        with open(img_path, 'wb+') as f:
+    context = {}
+    if request.method == 'POST' and request.FILES.get('image'):
+        img = request.FILES['image']    
+        img_path = os.path.join(settings.MEDIA_ROOT, img.name)
+
+        # Save the uploaded file to MEDIA_ROOT
+        with open(img_path, 'wb+') as destination:
             for chunk in img.chunks():
-                f.write(chunk)
-        image_array = image.img_to_array(image.load_img(img_path, target_size=(224, 224)))
-        image_array = np.expand_dims(image_array, axis=0)
-        predictions = model.predict(image_array)
+                destination.write(chunk)
+        
+        # Prepare the image for prediction
+        img = image.load_img(img_path, target_size=(224, 224))  # Adjust target_size based on your model
+        img_array = image.img_to_array(img)
+        img_array = np.expand_dims(img_array, axis=0)
+        img_array /= 255.0
+        # Make prediction
+        model = load_model(model_path)
+        predictions = model.predict(img_array)
         class_names = ['Actinic keratoses', 'Basal cell carcinoma', 'Benign keratosis-like lesions', 'Dermatofibroma', 'Melanoma', 'Melanocytic nevi', 'Vascular lesions']
         predicted_class_index = np.argmax(predictions[0])
         predicted_class_name = class_names[predicted_class_index]
-        result  = f'Predicted class: {predicted_class_name} with probability {predictions[0][predicted_class_index]:.4f}'
-        return JsonResponse({'result': result})
-    return render(request, 'app/upload.html')
+        result = f'Predicted class: {predicted_class_name} with probability {predictions[0][predicted_class_index]:.4f}'
+        
+        # Convert img_path to string for template rendering
+        context['result'] = result
+
+    return render(request, 'app/upload.html', context)
